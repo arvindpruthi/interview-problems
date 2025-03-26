98d9a328e413f27698f70174ad0c0e4725b4b7bc
Arvind Pruthi <apruthi@linkedin.com>
2025-03-26 02:51:28 +0000

    Doublets program

--- doublets.java
+++ doublets.java
@@ -10,9 +10,11 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class doublets {
-    static String[] dictionary = {"APE", "TAP", "OAT", "OAR", "MAN", "OPT", "APT", "ATE", "MAT"}
+    static String[] dictionary = new String[] {"APE", "TAP", "OAT", "OAR", "MAN", "OPT", "APT", "ATE", "MAT"};
     Map<String, List<String>> AdjGraph;
 
     private int distance(String from, String to) {
@@ -37,19 +39,72 @@ class doublets {
                 if (distance == 1) {
                     // There should be graph entry between these 2 nodes.
                     adj.computeIfAbsent(from, key -> new ArrayList<>());
-                    adj.computeIfPresent(from, (key, val) -> val.add(to));
-                    adj.computeIfAbsent(to, new ArrayList<>());
-                    adj.computeIfPresent(to, (key, val) -> val.add(from));
-                }                
+                    adj.computeIfPresent(from, (key, val) -> {
+                        val.add(to);
+                        return val;
+                    });
+                    adj.computeIfAbsent(to, key -> new ArrayList<>());
+                    adj.computeIfPresent(to, (key, val) -> {
+                        val.add(from);
+                        return val;
+                    });
+                }
             }
         }
-        for (String str : dictionary) {
-
-        }
+        return adj;
     }
 
     public doublets() {
+        this.AdjGraph = createAdjGraph();
+    }
+
+    public void printShortestDistance(String from, String to) {
+        if (from == to) {
+            System.out.println("Both from and to are the same words");
+            return;
+        }
+
+        Queue<String> q = new LinkedList<>();
+        Map<String, Integer> distance = new HashMap<>();
+        Map<String, String> shortestPath = new HashMap<>();
+        q.add(from);
+        int currentDistance = 0;
+        distance.put(from, currentDistance);
+        
+        while (!q.isEmpty()) {
+            String next = q.poll();
+            currentDistance = distance.get(next)+1;
+            for (String entry : AdjGraph.get(next)) {
+                // Update the distance here
+                Integer keyDistance = distance.get(entry);
+                if (keyDistance == null || keyDistance > currentDistance) {
+                    distance.put(entry, currentDistance);
+                    shortestPath.put(entry, next);
+                    q.add(entry);
+                }             
+            }
+        }
+
+        System.out.println("Distance from " + from + " and " + to + " is: " + distance.get(to));
+        System.out.println("Path is: ");
+        List<String> path = new ArrayList<>();
+        String entry = to; 
+        path.add(to);
+        while (entry != from) {
+            entry = shortestPath.get(entry);
+            path.add(entry);
+        }
+
+        for (int i=path.size()-1; i>0; i--) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println(to);
+
 
     }
 
+    public static void main(String[] args) {
+        doublets myDoublets = new doublets();
+        myDoublets.printShortestDistance("APE", "MAN");
+    }
 }
