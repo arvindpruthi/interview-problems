cb86fac0c16647cada87e77c50be971e94cc4a7c
Arvind Pruthi <apruthi@linkedin.com>
2025-03-30 23:24:59 +0000

    Update

--- MaxStackImpl.java
+++ MaxStackImpl.java
@@ -1,9 +1,12 @@
-public class MaxStackImpl<T> implements MaxStack<T> {
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class MaxStackImpl<T extends Comparable<T>> implements MaxStack<T> {
 
     static class LinkedListNode<T> {
         T val;
-        LinkedListNode left;
-        LinkedListNode right;
+        LinkedListNode<T> left;
+        LinkedListNode<T> right;
 
         public LinkedListNode(T val) {
             this.val = val;
@@ -20,8 +23,8 @@ public class MaxStackImpl<T> implements MaxStack<T> {
             head = tail = null;
         }
 
-        public LinkedListNode<T> insert(T val) {
-            LinkedListNode<T> newnode = new LinkedListNode(T);
+        public LinkedListNode<T> push(T val) {
+            LinkedListNode<T> newnode = new LinkedListNode<T>(val);
             if (head == null) {
                 head = tail = newnode;
             } else {
@@ -32,26 +35,88 @@ public class MaxStackImpl<T> implements MaxStack<T> {
             }
             return newnode;
         }
+
+        public LinkedListNode<T> pop() {
+            // Remove from tail
+            LinkedListNode<T> node = tail;
+            if (tail == null)
+                return null;
+            tail = tail.left;
+            if (tail == null)
+                head = null;
+            else {
+                tail.right = null;
+            }
+            return node;
+        }
+
+        public LinkedListNode<T> peek() {
+            LinkedListNode<T> node = tail;
+            return node;
+        }
+
+        public void delete(LinkedListNode<T> node) {
+            if (head == node) {
+                head = head.right;
+                if (head != null)
+                    head.left = null;
+            } else if (tail == node) {
+                tail = tail.left;
+                tail.right = null;
+            } else {
+                node.right.left = node.left;
+                node.left.right = node.right;
+                node.left = node.right = null;
+            }
+        }
     }
 
-    public MaxStackImpl() {
+    LinkedList<T> list;
+    PriorityQueue<LinkedListNode<T>> maxHeap;
 
+    public MaxStackImpl() {
+        list = new LinkedList<>();
+        maxHeap = new PriorityQueue<LinkedListNode<T>>((o1, o2) -> (o2.val - o1.val)); 
     }
 
    /** Add an element to the stack. */
-   public void push(T toPush);
+   public void push(T toPush) {
+        LinkedListNode<T> node = list.push(toPush);
+        maxHeap.add(node);
+   }
  
    /** Return the top value on the stack. */
-   public T peek();
+   public T peek() {
+        LinkedListNode<T> node = list.peek();
+        return node.val;
+   }
  
    /** Remove and return the top value from the stack. */
-   public T pop();
+   public T pop() {
+        LinkedListNode<T> node = list.pop();
+        if (node != null) {
+            maxHeap.remove(node);
+            return node.val;
+        }
+        return null;
+   }
     
    // Two special methods, so this isn't just 'implement a stack':
 
    /** Return the largest value in the stack. (Remember that T must implement Comparable.) */
-   public T peekMax();
+   public T peekMax() {
+        LinkedListNode<T> node = maxHeap.peek();
+        return node.val;
+   }
 
    /** Remove and return the largest value from the stack. */
-   public T popMax();
+   public T popMax() {
+        LinkedListNode<T> node = maxHeap.poll();
+        if (node != null) {
+            list.delete(node);
+            return node.val;
+        }
+        return null;
+    }
+
 }
